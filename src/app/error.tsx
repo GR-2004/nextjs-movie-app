1e14852b5a61abf0f536b244467ba45dedbfedd3
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-04-14 16:39:03 +0530

    fixing error component bug

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,16 +1,16 @@
-'use client'
-import { useEffect } from "react"
+import { useEffect } from "react";
 
-const error = ({ error, reset }:any) => {
-    useEffect(() => {
-        console.log(error);
-    }, [error]);
+const ErrorComponent = ({ error, reset }: any) => {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
   return (
     <div className="text-center mt-10">
       <h1>Something went wrong. Please try again later.</h1>
       <button className="hover:text-amber-500" onClick={() => reset()}>Try Again</button>
     </div>
-  )
-}
+  );
+};
 
-export default error
+export default ErrorComponent;
